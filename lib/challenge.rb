3af5fec941e4fda5e846b59b0ba2e41bd3031d9d
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-23 23:58:37 -0400

    Refactor solution
    -Refactor code to eliminate unnecessary local variables
    -Shorten methods by breaking out semantically distinct processes
    into separate methods
    -Run rspec to ensure that challenge.rb still passes all tests

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -27,30 +27,39 @@ def decode_morse(morse_code)
   end.join(' ')
 end
 
+# removes any leading and trailing zeroes from a binary string
 def trim_silence(bits)
   bits.sub!(/^0*/, '').sub!(/0*$/, '')
 end
 
-def parse_bits(bits)
-  # trim 'silence', i.e. remove any leading and trailing zeroes
-  bits = trim_silence(bits)
+# determines the shortest stretch of ones in a binary string
+def calculate_time_scale(bits)
+  bits.scan(/1+/).min_by(&:length).length
+end
 
-  # determine shortest stretch of ones: this is the time scale
-  time_scale = bits.scan(/1+/).min_by(&:length).length
-
-  # calibrate dots, dashes, and spaces using the time scale
-  dot = '1' * time_scale
+def split_bits_into_bit_words(bits, time_scale)
   word_space = '0000000' * time_scale
   char_space = '000' * time_scale
+  bits.split(word_space).map { |bit_word| bit_word.split(char_space) }
+end
+
+def convert_bit_char_to_morse_char(bit_char, time_scale)
+  dot = '1' * time_scale
   d_space = '0' * time_scale
+  bit_char.split(d_space).map do |d|
+    d == dot ? '.' : '-'
+  end.join
+end
+
+def parse_bits(bits)
+  bits = trim_silence(bits)
+  time_scale = calculate_time_scale(bits)
 
   # parse the bit string into dots & dashes and return a string of Morse code
-  bit_words = bits.split(word_space).map { |b_word| b_word.split(char_space) }
-  bit_words.map do |b_word|
-    b_word.map do |b_char|
-      b_char.split(d_space).map do |d|
-        d == dot ? '.' : '-'
-      end.join
+  bit_words = split_bits_into_bit_words(bits, time_scale)
+  bit_words.map do |bit_word|
+    bit_word.map do |bit_char|
+      convert_bit_char_to_morse_char(bit_char, time_scale)
     end.join(' ')
   end.join('   ')
 end
